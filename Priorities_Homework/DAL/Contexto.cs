62de5b03c42cca42dcc8790d1fffcc88bff8e034
DjMarte <alphat098@gmail.com>
2024-01-13 22:54:30 -0400

    Agregué el Contexto y las propiedaes en Models

---
+++ Priorities_Homework/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Priorities_Homework.Models;
+
+namespace Priorities_Homework.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Prioridades> Prioridades { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ Priorities_Homework/Models/Prioridades.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Priorities_Homework.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+
+        [Required(ErrorMessage = "Descripcion obligatoria")]
+        public string? Descripción { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public int DiasCompromiso { get; set; }
+
+
+
+    }
+}
