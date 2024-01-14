e084a0b7ba6b94541265368486fd9dc220ef7cdf
DjMarte <alphat098@gmail.com>
2024-01-14 16:01:04 -0400

    Agregué los métodos a la BLL

---
+++ Priorities_Homework/BLL/PrioridadesBLL.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Priorities_Homework.DAL;
+using Priorities_Homework.Models;
+using System.Linq.Expressions;
+
+namespace Priorities_Homework.BLL
+{
+    public class PrioridadesBLL
+    {
+        private Contexto _contexto;
+
+        public PrioridadesBLL(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public bool Existe(int prioridadId) {
+            return _contexto.Prioridades.Any(o => o.PrioridadId == prioridadId);
+
+        }
+
+        private bool Insertar(Prioridades prioridad) {
+            _contexto.Prioridades.Add(prioridad);
+            return _contexto.SaveChanges() > 0;
+        }
+
+        private bool Modificar(Prioridades prioridad) {
+            _contexto.Entry(prioridad).State = EntityState.Modified;
+            return _contexto.SaveChanges() > 0;
+        }
+
+        public bool Guardar(Prioridades prioridades) {
+            if (prioridades.PrioridadId == 0)
+                _contexto.Prioridades.Add(prioridades);
+            else
+                _contexto.Entry(prioridades).State = EntityState.Modified;
+
+            return _contexto.SaveChanges() > 0;
+        }
+        public Prioridades? Buscar(int prioridadId) {
+            return _contexto.Prioridades.Where(o => o.PrioridadId == prioridadId).
+                AsNoTracking().SingleOrDefault();
+        }
+
+        public bool Eliminar(int id) {
+            var prioridad = _contexto.Prioridades.Find(id);
+            _contexto.Prioridades.Remove(prioridad);
+            return _contexto.SaveChanges() > 0;
+
+        }
+
+        public List<Prioridades> ObtenerLista(Expression<Func<Prioridades, bool>> criterio) {
+            return _contexto.Prioridades
+                .AsNoTracking()
+                .Where(criterio)
+                .ToList();
+        }
+    }
+}
