c2e2d03c79f91b2e32785c144125cda7a2dbf621
DjMarte <alphat098@gmail.com>
2024-01-13 23:05:53 -0400

    Inyecté las dependencias

--- Priorities_Homework/Program.cs
+++ Priorities_Homework/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using Priorities_Homework.Components;
+using Priorities_Homework.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(ConStr));
+
 
 var app = builder.Build();
 
